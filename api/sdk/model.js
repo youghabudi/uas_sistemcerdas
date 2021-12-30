e789c8ebf0413df20e46d91eb22f9f4d5c3a132a
youghabudi <95616983+youghabudi@users.noreply.github.com>
2021-12-30 18:52:51 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -25,7 +25,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/ercooss/UAS_SC/main/public/dnn_model/model.json';
+        const path = 'https://raw.githubusercontent.com/youghabudi/uas_sistemcerdas/main/public/dnn_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
